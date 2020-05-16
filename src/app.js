61bbe0bc7ea3a1861c66ef7ac9611ff39f78b5c8
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2020-05-16 23:42:17 +0530

    table rows created

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import Header from './components/Header'
-import User from './components/User'
 
 async function App() {
   const template = document.createElement('template')
   template.innerHTML = `
     <div class="container">
       ${Header()}
-      ${await User()}
     </div>
   `
+  
   // Return a new node from template
   return template.content.cloneNode(true)
 }
